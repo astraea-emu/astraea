681518705bf16aec6b4fc696229ceed049f06df6
astraea-emu <331914284+astraea-emu@users.noreply.github.com>
2026-09-21 16:13:13 -0400

    M3 trace: define graphics Trace v0 adapters

---
+++ include/astraea/trace/graphics_v0.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <compare>
+#include <cstdint>
+
+#include <astraea/core/result.hpp>
+#include <astraea/graphics/graphics_ir.hpp>
+#include <astraea/graphics/packet.hpp>
+#include <astraea/graphics/rdna2_decoder.hpp>
+#include <astraea/graphics/shader_ir.hpp>
+#include <astraea/trace/v0.hpp>
+
+namespace astraea::trace {
+
+enum class GraphicsTraceAdapterErrorCodeV0 {
+    host_size_unrepresentable,
+    host_allocation_failure,
+};
+
+struct GraphicsTraceAdapterErrorV0 {
+    GraphicsTraceAdapterErrorCodeV0 code =
+        GraphicsTraceAdapterErrorCodeV0::
+            host_allocation_failure;
+
+    auto operator<=>(const GraphicsTraceAdapterErrorV0&) const =
+        default;
+};
+
+using GraphicsTraceEventResultV0 =
+    astraea::core::Result<
+        TraceEventV0,
+        GraphicsTraceAdapterErrorV0>;
+
+[[nodiscard]] GraphicsTraceEventResultV0
+trace_raw_graphics_packet_v0(
+    std::uint64_t event_id,
+    const astraea::graphics::RawPacket& packet);
+
+[[nodiscard]] GraphicsTraceEventResultV0
+trace_graphics_ir_v0(
+    std::uint64_t event_id,
+    const astraea::graphics::GraphicsIrEmission& emission);
+
+[[nodiscard]] GraphicsTraceEventResultV0
+trace_rdna2_decode_v0(
+    std::uint64_t event_id,
+    const astraea::graphics::Rdna2Instruction& instruction);
+
+[[nodiscard]] GraphicsTraceEventResultV0
+trace_shader_ir_v0(
+    std::uint64_t event_id,
+    const astraea::graphics::ShaderIrEmission& emission);
+
+}  // namespace astraea::trace
