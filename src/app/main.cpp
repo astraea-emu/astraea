f1ee51bb3cfcca5b0e84880f6ee1978f3f1dd528
astraea-emu <331914284+astraea-emu@users.noreply.github.com>
2026-09-24 19:08:59 -0400

    app: route CLI through supervised diagnostic entry

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,5 @@
-#include <astraea/core/version.hpp>
+#include "retail_diagnostic.hpp"
 
-#include <iostream>
-
-int main() {
-    std::cout << "Astraea " << astraea::core::version() << '\n';
-    std::cout << "M0 engineering foundation: no PS5 software execution is implemented yet.\n";
-    return 0;
+int main(int argc, char** argv) {
+    return astraea::app::run(argc, argv);
 }
